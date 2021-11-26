5bd633362ae3a69a55fab9203ad42181f038eaf1
Reid Liu <Reid.liu@kuka.com>
2021-11-26 14:36:53 +0800

    add grpc client

---
+++ grpc_learn/src/main/java/com/fantasybaby/examples/stream/GreeterMissionRequestClient.java
@@ -0,0 +1,39 @@
+package com.fantasybaby.examples.stream;
+
+import com.kuka.rcs.bd.grpc.*;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created on 10/3/2021.
+ *
+ * @author Reid Liu
+ */
+@Slf4j
+public class GreeterMissionRequestClient {
+    Channel channel = ManagedChannelBuilder
+            .forAddress("127.0.0.1", 7999)
+            .usePlaintext()
+            .build();
+    MissionServiceGrpc.MissionServiceBlockingStub missionServiceStub = MissionServiceGrpc.newBlockingStub(channel);
+
+    public void requestMissionUpdate() {
+        missionServiceStub.updateMissionWithSubmissions(MissionAndSubmissionRequest.newBuilder()
+                .setMission(Mission.newBuilder().setCode("m1").setStatus("FINISHED"))
+                .addSubmissions(Submission.newBuilder().setStatus("FINISHED").build())
+                .build());
+    }
+
+    public static void main(String[] args) {
+//        FINISHED
+        GreeterMissionRequestClient greeterMissionStreamClient = new GreeterMissionRequestClient();
+        greeterMissionStreamClient.requestMissionUpdate();
+    }
+}
--- grpc_learn/src/main/java/com/fantasybaby/examples/stream/GreeterMissionStreamClient.java
+++ grpc_learn/src/main/java/com/fantasybaby/examples/stream/GreeterMissionStreamClient.java
@@ -4,15 +4,15 @@ import com.kuka.rcs.bd.grpc.EmptyParameterRequest;
 import com.kuka.rcs.bd.grpc.MissionServiceGrpc;
 import com.kuka.rcs.bd.grpc.SubscribeMission;
 import com.kuka.rcs.bd.grpc.SubscribeMissionResponse;
-import com.kuka.rcs.bd.grpc.map.MapServiceGrpc;
-import com.kuka.rcs.bd.grpc.map.SubscribeMapRequest;
-import com.kuka.rcs.bd.grpc.map.SubscribeMapResponse;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created on 10/3/2021.
@@ -21,41 +21,60 @@ import java.util.concurrent.CountDownLatch;
  */
 @Slf4j
 public class GreeterMissionStreamClient {
+    CountDownLatch latch = new CountDownLatch(1);
+    Channel channel = ManagedChannelBuilder
+            .forAddress("127.0.0.1", 7999)
+            .usePlaintext()
+            .build();
+    MissionServiceGrpc.MissionServiceStub missionServiceStub = MissionServiceGrpc.newStub(channel);
+
     public static void main(String[] args) {
-        CountDownLatch latch = new CountDownLatch(1);
-        Channel channel = ManagedChannelBuilder
-                .forAddress("127.0.0.1", 7999)
-                .usePlaintext()
-                .build();
-        //异步存根
-        MissionServiceGrpc.MissionServiceStub missionServiceStub = MissionServiceGrpc.newStub(channel);
+
+        GreeterMissionStreamClient greeterMissionStreamClient = new GreeterMissionStreamClient();
+        CountDownLatch countDownLatch = greeterMissionStreamClient.runTest();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public CountDownLatch runTest() {
+        return subscribeMission();
+    }
+
+    public CountDownLatch subscribeMission() {
         StreamObserver<SubscribeMissionResponse> responseObserver = new StreamObserver<SubscribeMissionResponse>() {
             @Override
             public void onNext(SubscribeMissionResponse missionResponse) {
                 System.out.println("返回了结果\n");
                 SubscribeMission mission = missionResponse.getMission();
-                log.info("mission:{}",mission);
+                log.info("mission:{}", mission);
 
             }
 
             @Override
             public void onError(Throwable throwable) {
-                Thread.currentThread().interrupt();
-                latch.countDown();
+                log.error("error:{}", throwable);
+                try {
+                    TimeUnit.SECONDS.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                subscribeMission();
             }
 
             @Override
             public void onCompleted() {
+                log.error("on completed");
                 latch.countDown();
-
             }
         };
 
-        missionServiceStub.subscribeMissionStatus(EmptyParameterRequest.newBuilder().build(),responseObserver );
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        missionServiceStub.subscribeMissionStatus(EmptyParameterRequest.newBuilder().build(), responseObserver);
+        return latch;
     }
+
+
 }
